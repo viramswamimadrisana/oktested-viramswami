e5dec839cce83609efe26868fcaf54dc9fb6aff9
viramswamimadrisana <vadwaladhammadrisana@gmail.com>
2025-06-05 14:48:52 +0530

    Update PhotoGallery.tsx

--- components/PhotoGallery.tsx
+++ components/PhotoGallery.tsx
@@ -139,7 +139,8 @@ const PhotoGallery = () => {
         </motion.div>
 
         <div className="text-center mt-10">
-          <motion.button
+                 <motion.a
+            href="/videos"
             className="bg-gradient-to-r from-green-600 to-green-700 hover:from-green-700 hover:to-green-800 text-white font-semibold py-3 px-8 rounded-full transition-all duration-300 shadow-md hover:shadow-lg inline-flex items-center"
             whileHover={{ scale: 1.05 }}
             whileTap={{ scale: 0.95 }}
@@ -148,7 +149,7 @@ const PhotoGallery = () => {
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
               <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 0l-3 3a1 1 0 001.414 1.414L9 9.414V13a1 1 0 102 0V9.414l1.293 1.293a1 1 0 001.414-1.414z" clipRule="evenodd" />
             </svg>
-          </motion.button>
+          </motion.a>
         </div>
 
         {/* Lightbox Modal */}
